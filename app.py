0ab7f192c429f799c7e5c11f0ade6feea7c42620
saysamonegithub <115081195+saysamonegithub@users.noreply.github.com>
2023-04-11 09:33:22 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, render_template
+import openai
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index1.html')
+
+
+@app.route('/translate', methods=['POST'])
+def translate():
+    source_text = request.form['source_text']
+    source_lang = request.form['source_lang']
+    target_lang = request.form['target_lang']
+
+    openai.api_key = "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"
+    model_engine = "text-davinci-002"
+
+    response = openai.Completion.create(
+        engine=model_engine,
+        prompt="translate " + source_text + " from " + source_lang + " to " + target_lang,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    target_text = response['choices'][0]['text'].strip()
+
+    return render_template('index1.html', source_text=source_text, target_text=target_text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
